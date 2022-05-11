ab4943f3baca3ff091b580c032a158f934782ed8
Inae <inae9709@hanmail.net>
2022-05-11 22:53:04 +0900

    init: 초기 프로젝트 셋팅 (폴더링, 스보 삭제)

---
+++ challenge-async-await/challenge-async-await/Scene/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  challenge-async-await
+//
+//  Created by Devsisters on 2022/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		setupUI()
+	}
+
+	func setupUI() {
+		view.backgroundColor = .white
+	}
+}
---
+++ challenge-async-await/challenge-async-awaitUITests/challenge_async_awaitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  challenge_async_awaitUITestsLaunchTests.swift
+//  challenge-async-awaitUITests
+//
+//  Created by Devsisters on 2022/05/11.
+//
+
+import XCTest
+
+class challenge_async_awaitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
